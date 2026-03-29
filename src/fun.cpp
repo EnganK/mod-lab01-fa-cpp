a9d18b7c067f8a15b5c5f43c5e4e663977f85e61
EnganK <zuzbania@gmail.com>
2026-03-29 20:10:03 +0300

    Added functions

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,109 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+#include <cstring>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+
+bool isLatinLetter(char c)
+{
+	return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str)
+{
+	unsigned int count = 0;
+	bool inWord = false;
+	bool hasDigit = false;
+
+	for (int i = 0; str[i] != '\0'; ++i)
+	{
+		if (!isspace(str[i]))
+		{
+			inWord = true;
+			if (isdigit(str[i]))
+				hasDigit = true;
+		}
+		else
+		{
+			if (inWord && !hasDigit)
+				count++;
+			inWord = false;
+			hasDigit = false;
+		}
+	}
+
+	if (inWord && !hasDigit)
+		count++;
+	return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str)
+{
+	unsigned int count = 0;
+	bool inWord = false;
+	bool isValid = true;
+
+	for (int i = 0; str[i] != '\0'; ++i)
+	{
+		if (!isspace(str[i]))
+		{
+			if (!inWord)
+			{
+				if (isupper(str[i]) && isLatinLetter(str[i]))
+				{
+					inWord = true;
+					isValid = true;
+				}
+				else
+				{
+					inWord = false;
+					isValid = false;
+				}
+			}
+			else if (!islower(str[i]) || !isLatinLetter(str[i]))
+				isValid = false;
+		}
+		else
+		{
+			if (inWord && isValid)
+				count++;
+			inWord = false;
+			isValid = true;
+		}
+	}
+
+	if (inWord && isValid) {
+		count++;
+	}
+	return count;
+}
+
+unsigned int faStr3(const char* str)
+{
+	unsigned int totalLength = 0;
+	unsigned int wordCount = 0;
+	bool inWord = false;
+
+	for (int i = 0; str[i] != '\0'; ++i)
+	{
+		if (!isspace(str[i]))
+		{
+			if (!inWord)
+				inWord = true;
+			totalLength++;
+		}
+		else
+		{
+			if (inWord)
+			{
+				wordCount++;
+				inWord = false;
+			}
+		}
+	}
+	if (inWord)
+		wordCount++;
+	if (wordCount == 0)
+		return 0;
+	return (totalLength + wordCount / 2) / wordCount;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
-int main() {
-    return 0;
+int main()
+{
+	const char* testStr1 = "awfwaff 123 hjsakldfhl 13geg";
+	const char* testStr2 = "Awgawgg Awgawgawg wg Qsdgeag123 AWfgawga";
+	const char* testStr3 = "awfawg awff awag awgawg tfdbr";
+
+	std::cout << "Test faStr1: " << faStr1(testStr1) << std::endl;
+	std::cout << "Test faStr2: " << faStr2(testStr2) << std::endl;
+	std::cout << "Test faStr3: " << faStr3(testStr3) << std::endl;
+
+	return 0;
 }
